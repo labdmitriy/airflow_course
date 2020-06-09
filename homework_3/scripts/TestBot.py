002246692dbc232a99e36cfbb93a44f67a551957
Dmitry Labazkin <labdmitriy@gmail.com>
2020-06-09 19:17:12 +0000

    Add bot update

--- homework_3/scripts/TestBot.py
+++ homework_3/scripts/TestBot.py
@@ -1,5 +1,8 @@
 from typing import Union, Dict, Any
 from configparser import ConfigParser
+from pathlib import Path
+from operator import itemgetter
+from datetime import datetime
 
 import requests
 
@@ -11,15 +14,34 @@ class TestBot:
         self.token = token
         self.base_url = f'{self.BASE_URL}/bot{token}'
 
+    def _get_offset(self, chat_id: Union[str, int]) -> int:
+        get_update_id = itemgetter('update_id')
+        current_update = self.get_updates(timeout=3)
+
+        print('current update', current_update)
+
+        update_ids = list(map(get_update_id, current_update['result']))
+
+        if len(update_ids) > 0:
+            offset = max(update_ids) + 1
+        else:
+            offset = 0
+
+        print(offset)
+        return offset
+
     def send_message(
         self,
         chat_id: Union[str, int],
         text: str,
         include_button: bool = False,
         button_text: str = '',
+        reporter_name: str = '',
         parse_mode: str = 'Markdown',
         disable_notification: bool = True,
     ) -> Dict:
+
+        self.offset = self._get_offset(chat_id)
 
         payload: Dict[str, Any] = {
             'chat_id': chat_id,
@@ -31,41 +53,59 @@ class TestBot:
         if include_button:
             go_button = {
                 'text': button_text,
-                'callback_data': 'callback data'
+                'callback_data': reporter_name
             }
             go_keyboard = {'inline_keyboard': [[go_button]]}
             payload['reply_markup'] = go_keyboard
 
-        print(payload)
-
         response = requests.post(
-            f'{self.base_url}/sendMessage', json=payload, timeout=3
+            f'{self.base_url}/sendMessage', json=payload, timeout=5
         )
         return response.json()
 
-    def get_updates(self) -> Dict:
+    def get_updates(self, offset: int = 0, timeout: int = 0) -> Dict:
         payload = {
-            'allowed_updates': ['callback_query'],
-            # 'limit': 2
+            # 'allowed_updates': ['callback_query'],
+            'timeout': timeout,
+            'offset': offset
         }
         response = requests.post(
-            f'{self.base_url}/getUpdates', json=payload, timeout=3
+            f'{self.base_url}/getUpdates', json=payload, timeout=5
         )
         return response.json()
 
 
-config = ConfigParser()
-config.read('./config/config.ini')
+CONFIG_PATH = Path(__file__).parent.absolute()/Path('../config')
 
-TEST_TOKEN = config['default']['TEST_TOKEN']
-TEST_CHAT_ID = config['default']['TEST_CHAT_ID']
+config = ConfigParser()
+config.read(CONFIG_PATH/'config.ini')
+
+TEST_TOKEN = config['DEFAULT']['test_token']
+TEST_CHAT_ID = config['DEFAULT']['test_chat_id']
 
 bot = TestBot(token=TEST_TOKEN)
 msg_response = bot.send_message(TEST_CHAT_ID, text='Привет',
-                                include_button=True, button_text='Поехали')
+                                include_button=True, button_text='Поехали',
+                                reporter_name='labdmitriy')
 print('message response')
 print(msg_response)
 
-update_response = bot.get_updates()
+print('offset')
+print(bot.offset)
+
+update_response = bot.get_updates(offset=bot.offset, timeout=3)
 print('update response')
 print(update_response)
+
+# db_record = filter(lambda x: x)
+result = update_response['result']
+
+if len(result) > 0:
+    callback_query = result[0]['callback_query']
+    db_record = {}
+    db_record['chat_id'] = TEST_CHAT_ID
+    db_record['username'] = callback_query['from']['id']
+    db_record['trigerred_at'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    db_record['event_type'] = 'click'
+    db_record['reporter_name'] = callback_query['data']
+    print(db_record)
