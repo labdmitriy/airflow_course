48bfdc56bf03389353d3a8b6f7e7aa6659fa1e52
Dmitry Labazkin <labdmitriy@gmail.com>
2020-06-24 16:32:45 +0000

    Prepare processing for DAG run

--- project/dags/project.py
+++ project/dags/project.py
@@ -1,14 +1,18 @@
-import sys
 import json
-from typing import Dict, List
+import sys
+from itertools import chain
 from pathlib import Path
+from typing import Dict, List
+from multiprocessing import Pool
 
 from airflow import DAG
 from airflow.models import Variable
 from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
 
 from merch.operators import TemplatedPythonOperator
 sys.path.insert(1, '/home/jupyter/lib/scrape')
+from scrape.parsers import get_domain_counts
 from scrape.sheets import GoogleSheet
 from scrape.validators import URLValidator
 
@@ -23,6 +27,7 @@ URL_COL_NAME = 'ссылка'
 TEAM_COL_NAME = 'Лабазкин Дмитрий & Хачатрян Екатерина'
 
 VALIDATION_RESULTS_PATH = DATA_PATH/'url_validation_results.json'
+PARSE_RESULTS_PATH = DATA_PATH/'url_parse_results.json'
 
 VALID_DOMAINS = {
     'habr.com', 'pikabu.ru', 'pornhub.com',
@@ -72,6 +77,30 @@ def process_urls(
             f.write(json.dumps(result) + '\n')
 
 
+def parse_urls(
+    domains: List,
+    validation_results_path: Path,
+    parse_results_path: Path
+) -> None:
+    valid_results = []
+
+    with open(validation_results_path) as f:
+        for line in f:
+            valid_results.append(json.loads(line))
+
+    args = [(valid_results, domain) for domain in domains]
+    cpu_count = len(domains)
+
+    with Pool(cpu_count) as p:
+        domain_results = p.starmap(get_domain_counts, args)
+
+    parse_results = list(chain(*domain_results))
+
+    with open(parse_results_path, 'w') as f:
+        for result in parse_results:
+            f.write(json.dumps(result) + '\n')
+
+
 process_urls_task = TemplatedPythonOperator(
     task_id='process_urls',
     python_callable=process_urls,
@@ -90,6 +119,18 @@ process_urls_task = TemplatedPythonOperator(
     dag=dag
 )
 
+parse_urls_task = PythonOperator(
+    task_id='parse_urls',
+    python_callable=parse_urls,
+    op_kwargs={
+        'domains': VALID_DOMAINS,
+        'validation_results_path': VALIDATION_RESULTS_PATH,
+        'parse_results_path': PARSE_RESULTS_PATH
+    },
+    dag=dag
+)
+
+process_urls_task >> parse_urls_task
 
 if __name__ == '__main__':
     dag.clear(reset_dag_runs=True)
